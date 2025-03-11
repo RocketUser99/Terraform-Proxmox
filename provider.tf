301930dc4c7526948f925abcc02130ff084edb30
Tou Yang <72373027+RocketUser99@users.noreply.github.com>
2025-03-10 18:28:22 -0700

    Added proxmox provider

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  
+  required_version = ">= 0.36.4"
+  
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
+# Proxmox use self-sign certificate so pm_tls_insecure is set to true
+
+provider "proxmox" {
+  pm_api_url = var.proxmox_url
+  pm_api_token_id = var.tokenid
+  pm_api_token_secret = var.terra_token
+  pm_tls_insecure = true
+
+}
